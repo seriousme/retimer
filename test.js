94bfc09840809c908732d3d153a246f4114b8d04
Hans Klunder <hans.klunder@gmail.com>
2025-05-24 17:19:44 +0000

    chore:modernize dependencies

--- test.js
+++ test.js
@@ -1,117 +1,122 @@
 'use strict'
 
-const test = require('tape')
+const test = require('node:test')
 const retimer = require('./')
 
-test('schedule a callback', function (t) {
+test('schedule a callback', async function (t) {
   t.plan(1)
 
-  const start = Date.now()
+  await new Promise(resolve => {
+    const start = Date.now()
 
-  retimer(function () {
-    t.ok(Date.now() - start >= 50, 'it was deferred ok!')
-  }, 50)
+    retimer(function () {
+      t.assert.ok(Date.now() - start >= 50, 'it was deferred ok!')
+      resolve()
+    }, 50)
+  })
 })
 
-test('reschedule a callback', function (t) {
+test('reschedule multiple times', async function (t) {
   t.plan(1)
 
-  const start = Date.now()
+  await new Promise(resolve => {
+    const start = Date.now()
 
-  const timer = retimer(function () {
-    t.ok(Date.now() - start >= 70, 'it was deferred ok!')
-  }, 50)
+    const timer = retimer(function () {
+      t.assert.ok(Date.now() - start >= 90, 'it was deferred ok!')
+      resolve()
+    }, 50)
 
-  setTimeout(function () {
-    timer.reschedule(50)
-  }, 20)
-})
-
-test('reschedule multiple times', function (t) {
-  t.plan(1)
-
-  const start = Date.now()
-
-  const timer = retimer(function () {
-    t.ok(Date.now() - start >= 90, 'it was deferred ok!')
-  }, 50)
-
-  setTimeout(function () {
-    timer.reschedule(50)
     setTimeout(function () {
       timer.reschedule(50)
+      setTimeout(function () {
+        timer.reschedule(50)
+      }, 20)
     }, 20)
-  }, 20)
+  })
 })
 
-test('clear a timer', function (t) {
+test('clear a timer', async function (t) {
   t.plan(1)
+  await new Promise((resolve) => {
+    const timer = retimer(function () {
+      t.assert.ok(false, 'the timer should never get called')
+    }, 20)
 
-  const timer = retimer(function () {
-    t.fail('the timer should never get called')
-  }, 20)
+    timer.clear()
 
-  timer.clear()
-
-  setTimeout(function () {
-    t.pass('nothing happened')
-  }, 50)
-})
-
-test('clear a timer after a reschedule', function (t) {
-  t.plan(1)
-
-  const timer = retimer(function () {
-    t.fail('the timer should never get called')
-  }, 20)
-
-  setTimeout(function () {
-    timer.reschedule(50)
     setTimeout(function () {
-      timer.clear()
-    }, 10)
-  }, 10)
-
-  setTimeout(function () {
-    t.pass('nothing happened')
-  }, 50)
+      t.assert.ok(true, 'nothing happened')
+      resolve()
+    }, 50)
+  })
 })
 
-test('can be rescheduled early', function (t) {
+test('clear a timer after a reschedule', async function (t) {
+  t.plan(1)
+  await new Promise((resolve) => {
+    const timer = retimer(function () {
+      t.assert.ok(false, 'the timer should never get called')
+    }, 20)
+
+    setTimeout(function () {
+      timer.reschedule(50)
+      setTimeout(function () {
+        timer.clear()
+      }, 10)
+    }, 10)
+
+    setTimeout(function () {
+      t.assert.ok(true, 'nothing happened')
+      resolve()
+    }, 50)
+  })
+})
+
+test('can be rescheduled early', async function (t) {
   t.plan(1)
 
-  const start = Date.now()
+  await new Promise((resolve) => {
+    const start = Date.now()
 
-  const timer = retimer(function () {
-    t.ok(Date.now() - start <= 500, 'it was rescheduled!')
-  }, 500)
+    const timer = retimer(function () {
+      t.assert.ok(Date.now() - start <= 500, 'it was rescheduled!')
+      resolve()
+    }, 500)
 
-  setTimeout(function () {
-    timer.reschedule(10)
-  }, 20)
+    setTimeout(function () {
+      timer.reschedule(10)
+    }, 20)
+  })
 })
 
-test('can be rescheduled even if the timeout has already triggered', function (t) {
+test('can be rescheduled even if the timeout has already triggered', async function (t) {
   t.plan(2)
 
-  const start = Date.now()
-  let count = 0
+  await new Promise((resolve) => {
+    const start = Date.now()
+    let count = 0
 
-  const timer = retimer(function () {
-    count++
-    if (count === 1) {
-      t.ok(Date.now() - start >= 20, 'it was triggered!')
-      timer.reschedule(20)
-    } else {
-      t.ok(Date.now() - start >= 40, 'it was rescheduled!')
-    }
-  }, 20)
+    const timer = retimer(function () {
+      count++
+      if (count === 1) {
+        t.assert.ok(Date.now() - start >= 20, 'it was triggered!')
+        timer.reschedule(20)
+      } else {
+        t.assert.ok(Date.now() - start >= 40, 'it was rescheduled!')
+        resolve()
+      }
+    }, 20)
+  })
 })
 
-test('pass arguments to the callback', function (t) {
+test('pass arguments to the callback', async function (t) {
   t.plan(1)
 
-  retimer(function (arg) {
-    t.equal(arg, 42, 'argument matches')
-  }, 50, 42)
+  await new Promise((resolve) => {
+    retimer(function (arg) {
+      t.assert.equal(arg, 42, 'argument matches')
+      resolve()
+    }, 50, 42)
+  })
 })
